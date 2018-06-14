2fb2775a24f636bc2b4e74e7f5946ff91214a75a
EngleLin <engle0232@gmail.com>
2018-06-14 16:18:42 +0800

    add and delete

--- src/index.js
+++ src/index.js
@@ -2,28 +2,70 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class ShopItem extends React.Component {
+
+	clickDel(){  //對照一下剛剛在 constructor 中 bind 的形式
+		this.props.delItem( this.props.index );
+	}
+	
 	render() {
-		return(
-		<tr>
-		<td>aaa</td>
-		<td>bbb</td>
-		</tr>
-	)
-
-	}	
-}
-
-class App extends React.Component{
-	render(){
-		return(
-		<table>
-		<tbody>
-		<ShopItem />
-		<ShopItem />
-		</tbody>
-		</table>
+		return (
+			<tr>
+				<td>{this.props.value}</td>
+				<td><button onClick={this.clickDel.bind(this)}>刪除</button></td>
+			</tr>
 		)
 	}
 }
 
-ReactDom.render(<App />,document.getElementById('root'));
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: ['QOQ','QWQ','QUQ','QAQ'],  //呈現用的清單
+			sample: ''
+		};
+		this.testClick = this.testClick.bind(this);
+		this.changeText = this.changeText.bind(this);
+	}
+	
+	changeText( event ) {
+		this.setState( {sample: event.target.value}  );
+	}
+	
+	testClick() {
+		const data = this.state.items;
+		data.push( this.state.sample );
+		this.setState( {
+			items: data,
+			sample: ''
+		} );
+	}
+	
+	delItem(index){
+		const data = this.state.items;
+		data.splice(index, 1);
+		this.setState( { items: data } );
+	}
+	
+	render() {
+		return (
+			<div>
+				<input type="text" value={this.state.sample} 
+					onChange={this.changeText} />
+				<button onClick={this.testClick}>增加</button>
+				
+				<table>
+					<tbody>
+						{this.state.items.map( (aa,idx) => (
+							<ShopItem value={aa} index={idx} 
+								delItem={this.delItem.bind(this)} />
+						))}
+					</tbody>
+				</table>
+			</div>
+		)
+	}
+}
+
+ReactDom.render( <App />, document.getElementById('root') );
